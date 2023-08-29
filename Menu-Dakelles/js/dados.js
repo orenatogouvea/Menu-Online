819fdc7f8b01f23c552b1dc9eafdd29a420e2d5d
orenatogouvea <112201035+orenatogouvea@users.noreply.github.com>
2023-08-29 16:57:17 -0300

    Add files via upload

---
+++ Menu-Dakelles/js/dados.js
@@ -0,0 +1,78 @@
+var MENU = {
+    "trufa": [
+        {
+            "id": "trufa-dois-amores",
+            "img": "./img/cardapio/trufa/dois-amores.png",
+            "name": "Dois Amores (Trufa)",
+            "dsc": "Dois Amores (Trufa)",
+            "price": 3.50
+        },
+        {
+            "id": "trufa-cereja",
+            "img": "./img/cardapio/trufa/cereja.png",
+            "name": "Cereja (Trufa)",
+            "dsc": "Cereja (Trufa)",
+            "price": 3.50
+        },
+        {
+            "id": "trufa-limao",
+            "img": "./img/cardapio/trufa/limao.png",
+            "name": "Limão (Trufa)",
+            "dsc": "Limão (Trufa)",
+            "price": 3.50
+        },
+    ],
+    "paodemel": [
+        {
+            "id": "pao-de-mel-doce-de-leite",
+            "img": "./img/cardapio/paodemel/doce-de-leite.png",
+            "name": "Doce de leite (Pão de mel)",
+            "dsc": "Doce de leite (Pão de mel)",
+            "price": 6
+        },
+        {
+            "id": "pao-de-mel-brigadeiro",
+            "img": "./img/cardapio/paodemel/brigadeiro.png",
+            "name": "Brigadeiro (Pão de mel)",
+            "dsc": "Brigadeiro (Pão de mel)",
+            "price": 6
+        },
+        {
+            "id": "pao-de-mel-beijinho",
+            "img": "./img/cardapio/paodemel/beijinho.png",
+            "name": "Beijinho (Pão de mel)",
+            "dsc": "Beijinho (Pãp de mel)",
+            "price": 6
+        },
+        {
+            "id": "pao-de-mel-ninho",
+            "img": "./img/cardapio/paodemel/leite-ninho.png",
+            "name": "Leite Ninho (Pão de mel)",
+            "dsc": "Leite Ninho (Pão de mel)",
+            "price": 6
+        },
+        {
+            "id": "pao-de-mel-amendoim",
+            "img": "./img/cardapio/paodemel/amendoim.png",
+            "name": "Amendoim (Pão de mel)",
+            "dsc": "Amendoim (Pão de mel)",
+            "price": 8
+        },
+    ],
+    "cone": [
+        {
+            "id": "cone-doce-de-leite",
+            "img": "./img/cardapio/cone/doce-de-leite.png",
+            "name": "Doce de Leite (Cone)",
+            "dsc": "Doce de Leite (Cone)",
+            "price": 8
+        },
+        {
+            "id": "cone-brigadeiro",
+            "img": "./img/cardapio/cone/brigadeiro.png",
+            "name": "Brigadeiro (Cone)",
+            "dsc": "Brigadeiro (Cone)",
+            "price": 8
+        },
+    ],
+}
