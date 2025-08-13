d75e242e383832afc4b720cb33d77362790ba6fc
orenatogouvea <112201035+orenatogouvea@users.noreply.github.com>
2025-08-13 18:59:54 -0300

    new-att

--- Menu-Dakelles/js/dados.js
+++ Menu-Dakelles/js/dados.js
@@ -5,21 +5,42 @@ var MENU = {
             "img": "./img/cardapio/trufa/dois-amores.png",
             "name": "Dois Amores (Trufa)",
             "dsc": "Dois Amores (Trufa)",
-            "price": 4
+            "price": 5
         },
         {
-            "id": "trufa-cereja",
-            "img": "./img/cardapio/trufa/cereja.png",
-            "name": "Cereja (Trufa)",
-            "dsc": "Cereja (Trufa)",
-            "price": 4
+            "id": "trufa-beijinho",
+            "img": "./img/cardapio/trufa/trufa-beijinho.png",
+            "name": "Beijinho (Trufa)",
+            "dsc": "Beijinho (Trufa)",
+            "price": 5
         },
         {
             "id": "trufa-limao",
             "img": "./img/cardapio/trufa/limao.png",
             "name": "Limão (Trufa)",
             "dsc": "Limão (Trufa)",
-            "price": 4
+            "price": 5
+        },
+        {
+            "id": "trufa-ninho",
+            "img": "./img/cardapio/trufa/trufa-ninho.png",
+            "name": "Ninho (Trufa)",
+            "dsc": "Ninho (Trufa)",
+            "price": 5
+        },
+        {
+            "id": "trufa-brigadeiro",
+            "img": "./img/cardapio/trufa/trufa-brigadeiro.png",
+            "name": "Brigadeiro (Trufa)",
+            "dsc": "Brigadeiro (Trufa)",
+            "price": 5
+        },
+        {
+            "id": "trufa-doce-de-leite",
+            "img": "./img/cardapio/trufa/trufa-doce-de-leite.png",
+            "name": "Doce de Leite (Trufa)",
+            "dsc": "Doce de Leite (Trufa)",
+            "price": 5
         },
     ],
     "paodemel": [
@@ -28,51 +49,109 @@ var MENU = {
             "img": "./img/cardapio/paodemel/doce-de-leite.png",
             "name": "Doce de leite (Pão de mel)",
             "dsc": "Doce de leite (Pão de mel)",
-            "price": 6
+            "price": 8
         },
         {
             "id": "pao-de-mel-brigadeiro",
             "img": "./img/cardapio/paodemel/brigadeiro.png",
             "name": "Brigadeiro (Pão de mel)",
             "dsc": "Brigadeiro (Pão de mel)",
-            "price": 6
+            "price": 8
         },
         {
             "id": "pao-de-mel-beijinho",
             "img": "./img/cardapio/paodemel/beijinho.png",
             "name": "Beijinho (Pão de mel)",
             "dsc": "Beijinho (Pãp de mel)",
-            "price": 6
+            "price": 8
         },
         {
             "id": "pao-de-mel-ninho",
             "img": "./img/cardapio/paodemel/leite-ninho.png",
             "name": "Leite Ninho (Pão de mel)",
             "dsc": "Leite Ninho (Pão de mel)",
-            "price": 6
-        },
-        {
-            "id": "pao-de-mel-amendoim",
-            "img": "./img/cardapio/paodemel/amendoim.png",
-            "name": "Amendoim (Pão de mel)",
-            "dsc": "Amendoim (Pão de mel)",
             "price": 8
         },
     ],
-    "cone": [
+    "barra": [
         {
-            "id": "cone-doce-de-leite",
-            "img": "./img/cardapio/cone/doce-de-leite.png",
-            "name": "Doce de Leite (Cone)",
-            "dsc": "Doce de Leite (Cone)",
-            "price": 8
+            "id": "barra-promo",
+            "img": "./img/cardapio/barra/barra-promo.png",
+            "name": "2 Barras por menos",
+            "dsc": "Barra da Felicidade",
+            "price": 42
         },
         {
-            "id": "cone-brigadeiro",
-            "img": "./img/cardapio/cone/brigadeiro.png",
-            "name": "Brigadeiro (Cone)",
-            "dsc": "Brigadeiro (Cone)",
-            "price": 8
+            "id": "barra-red-velvet",
+            "img": "./img/cardapio/barra/red-velvet.png",
+            "name": "Velvet",
+            "dsc": "Barra da Felicidade",
+            "price": 30
+        },
+        {
+            "id": "barra-morango",
+            "img": "./img/cardapio/barra/barra-morango.png",
+            "name": "Morango",
+            "dsc": "Barra da Felicidade",
+            "price": 25
+        },
+        {
+            "id": "barra-morango-uva",
+            "img": "./img/cardapio/barra/barra-morango-uva.png",
+            "name": "Mesclada",
+            "dsc": "Barra da Felicidade",
+            "price": 25
+        },
+        {
+            "id": "barra-uva",
+            "img": "./img/cardapio/barra/barra-uva.png",
+            "name": "Uva",
+            "dsc": "Barra da Felicidade",
+            "price": 25
+        },
+    ],
+    "bolo": [
+        {
+            "id": "bolo-brigadeiro",
+            "img": "./img/cardapio/bolo/bolo-brigadeiro.png",
+            "name": "Brigadeiro (Bolo)",
+            "dsc": "Brigadeiro (Bolo)",
+            "price": 60
+        },
+        {
+            "id": "bolo-pacoca",
+            "img": "./img/cardapio/bolo/bolo-pacoca.png",
+            "name": "Paçoca (Bolo)",
+            "dsc": "Paçoca (Bolo)",
+            "price": 60
+        },
+        {
+            "id": "bolo-pao-de-mel",
+            "img": "./img/cardapio/bolo/bolo-pao-de-mel.png",
+            "name": "Pão de mel (Bolo)",
+            "dsc": "Pão de mel (Bolo)",
+            "price": 70
+        },
+        {
+            "id": "bolo-ninho-nutella",
+            "img": "./img/cardapio/bolo/bolo-ninho-nutella.png",
+            "name": "Ninho c/ Nutella (Bolo)",
+            "dsc": "Ninho c/ Nutella (Bolo)",
+            "price": 65
+        },
+        {
+            "id": "bolo-milho",
+            "img": "./img/cardapio/bolo/bolo-milho.png",
+            "name": "Milho (Bolo)",
+            "dsc": "Milho (Bolo)",
+            "price": 50
+        },
+        {
+            "id": "bolo-canela",
+            "img": "./img/cardapio/bolo/bolo-canela.png",
+            "name": "Canela c/ Açucar (Bolo)",
+            "dsc": "Canela c/ Açucar (Bolo)",
+            "price": 30
         },
     ],
 }
